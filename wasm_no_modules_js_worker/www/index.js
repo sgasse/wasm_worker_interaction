6effbdd975f783a2c6b7c62391f2732d07af1d4f
Simon Gasse <sgasse@users.noreply.github.com>
2021-05-13 09:07:40 +0200

    Add no-modules example

---
+++ wasm_no_modules_js_worker/www/index.js
@@ -0,0 +1,16 @@
+const {add_two_numbers} = wasm_bindgen;
+
+async function run_wasm() {
+    // Load the wasm file by awaiting the Promise returned by `init()` to resolve
+    await wasm_bindgen('./pkg/wasm_no_modules_js_worker_bg.wasm');
+
+    console.log('index.js loaded');
+
+    // Demonstrate that we can call our function imported from wasm
+    console.log('2 + 4 = ', add_two_numbers(2, 4));
+
+    // Create a worker in JS - no option `{type: 'module'}`
+    var myWorker = new Worker('./worker.js');
+}
+
+run_wasm();
