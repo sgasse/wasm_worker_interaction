ea9c383747dd95e8542f0f3302cd17ad2f8887e5
Simon Gasse <sgasse@users.noreply.github.com>
2021-05-13 13:45:40 +0200

    Add documentation to interaction example

--- wasm_worker_interaction/src/lib.rs
+++ wasm_worker_interaction/src/lib.rs
@@ -4,6 +4,11 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{console, HtmlElement, HtmlInputElement, MessageEvent, Worker};
 
+/// A number evaluation struct
+/// 
+/// This struct will be the main object which responds to messages passed to the worker. It stores
+/// the last number which it was passed to have a state. The statefulness is not is not required in
+/// this example but should show how larger, more complex scenarios with statefulness can be set up.
 #[wasm_bindgen]
 pub struct NumberEval {
     number: i32,
@@ -11,14 +16,17 @@ pub struct NumberEval {
 
 #[wasm_bindgen]
 impl NumberEval {
+    /// Create new instance.
     pub fn new(init_number: i32) -> NumberEval {
         NumberEval{number: init_number}
     }
 
+    /// Get last number that was checked - this method is added to work with statefulness.
     pub fn get_last_number(&self) -> i32 {
         self.number
     }
 
+    /// Check if a number is even and store it as last processed number.
     pub fn is_even(&mut self, number: i32) -> bool {
         self.number = number;
         match self.number % 2 {
@@ -27,40 +35,26 @@ impl NumberEval {
         }
     }
 }
+
+/// Run entry point for the main thread.
 #[wasm_bindgen]
 pub fn startup() {
-    // Until deploying a WASM application in production, adding the panic hook
-    // makes debugging a lot easier.
+    // This is not strictly needed but makes debugging a lot easier.
+    // Should not be used in productive deployments.
     set_panic_hook();
 
-
+    // Here, we create our worker. In a larger app, multiple callbacks should be able to interact
+    // with the code in the worker. Therefore, we wrap it in `Rc<RefCell>` following the interior
+    // mutability pattern. In this example, it would not be needed but we include the wrapping
+    // anyway as example.
     let worker_handle = Rc::new(RefCell::new(Worker::new("./worker.js").unwrap()));
     console::log_1(&"Created a new worker from within WASM".into());
 
+    // Pass the worker to the function which sets up the `onchange` callback.
     setup_input_onchange_callback(worker_handle.clone());
 }
 
-fn get_on_msg_callback() -> Closure<dyn FnMut(MessageEvent)> {
-    let callback = Closure::wrap(Box::new(move |event: MessageEvent | {
-        console::log_2(&"Received response: ".into(), &event.data().into());
 
-        let document = web_sys::window().unwrap().document().unwrap();
-
-        let result = match event.data().as_bool().unwrap() {
-            true => "even",
-            false => "odd",
-        };
-
-        document
-            .get_element_by_id("resultField")
-            .expect("#resultField should exist")
-            .dyn_ref::<HtmlElement>()
-            .expect("#resultField should be a HtmlInputElement")
-            .set_inner_text(result);
-    }) as Box<dyn FnMut(_)>);
-
-    callback
-}
 
 fn setup_input_onchange_callback(worker: Rc<RefCell<web_sys::Worker>>) {
     let document = web_sys::window().unwrap().document().unwrap();
@@ -82,11 +76,17 @@ fn setup_input_onchange_callback(worker: Rc<RefCell<web_sys::Worker>>) {
         let input_field = input_field.dyn_ref::<HtmlInputElement>()
             .expect("#inputNumber should be a HtmlInputElement");
 
+        // If the value in the field can be parsed to a `i32`, send it to the worker. Otherwise
+        // clear the result field.
         match input_field.value().parse::<i32>() {
             Ok(number) => {
+                // Access worker behind shared handle, following the interior mutability pattern.
                 let worker_handle = &*worker.borrow();
                 let _ = worker_handle.post_message(&number.into());
                 persistent_callback_handle = get_on_msg_callback();
+
+                // Since the worker returns the message asynchronously, we attach a callback to be
+                // triggered when the worker returns.
                 worker_handle.set_onmessage(Some(persistent_callback_handle.as_ref().unchecked_ref()));
             },
             Err(_) => {
@@ -101,6 +101,7 @@ fn setup_input_onchange_callback(worker: Rc<RefCell<web_sys::Worker>>) {
 
     }) as Box<dyn FnMut()>);
 
+    // Attach the closure as `onchange` callback to the input field.
     document
         .get_element_by_id("inputNumber")
         .expect("#inputNumber should exist")
@@ -108,12 +109,33 @@ fn setup_input_onchange_callback(worker: Rc<RefCell<web_sys::Worker>>) {
         .expect("#inputNumber should be a HtmlInputElement")
         .set_oninput(Some(callback.as_ref().unchecked_ref()));
 
-    // Leaks memory
+    // Leaks memory.
     callback.forget();
 }
 
+/// Create a closure to act on the message returned by the worker
+fn get_on_msg_callback() -> Closure<dyn FnMut(MessageEvent)> {
+    let callback = Closure::wrap(Box::new(move |event: MessageEvent | {
+        console::log_2(&"Received response: ".into(), &event.data().into());
 
+        let result = match event.data().as_bool().unwrap() {
+            true => "even",
+            false => "odd",
+        };
 
+        let document = web_sys::window().unwrap().document().unwrap();
+        document
+            .get_element_by_id("resultField")
+            .expect("#resultField should exist")
+            .dyn_ref::<HtmlElement>()
+            .expect("#resultField should be a HtmlInputElement")
+            .set_inner_text(result);
+    }) as Box<dyn FnMut(_)>);
+
+    callback
+}
+
+/// Set a hook to log a panic stack trace in JS.
 pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
--- wasm_worker_interaction/www/index.js
+++ wasm_worker_interaction/www/index.js
@@ -1,12 +1,17 @@
+// We only need `startup` here which is the main entry point
+// In theory, we could also use all other functions/struct types from Rust which we have bound with
+// `#[wasm_bindgen]`
 const {startup} = wasm_bindgen;
 
 async function run_wasm() {
-    // Load the wasm file by awaiting the Promise returned by `init()` to resolve
+    // Load the wasm file by awaiting the Promise returned by `wasm_bindgen`
+    // `wasm_bindgen` was imported in `index.html`
     await wasm_bindgen('./pkg/wasm_worker_interaction_bg.wasm');
 
     console.log('index.js loaded');
 
-    // Run main WASM entry point which will create a worker
+    // Run main WASM entry point
+    // This will create a worker from within our Rust code compiled to WASM
     startup();
 }
 
