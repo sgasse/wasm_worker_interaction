15ed646f70bed4ea042843739ded05d898dc1bee
Simon Gasse <sgasse@users.noreply.github.com>
2021-05-13 10:32:28 +0200

    Make worker act on callback

--- wasm_worker_interaction/src/lib.rs
+++ wasm_worker_interaction/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{console, HtmlInputElement, Worker};
@@ -13,16 +15,34 @@ pub fn startup() {
     // makes debugging a lot easier.
     set_panic_hook();
 
-    setup_input_onchange_callback();
 
-    Worker::new("./worker.js");
+    let worker_handle = Rc::new(RefCell::new(Worker::new("./worker.js").unwrap()));
     console::log_1(&"Created a new worker from within WASM".into());
+
+    setup_input_onchange_callback(worker_handle.clone());
 }
 
-fn setup_input_onchange_callback() {
+fn setup_input_onchange_callback(worker: Rc<RefCell<web_sys::Worker>>) {
     let document = web_sys::window().unwrap().document().unwrap();
 
     let callback = Closure::wrap(Box::new(move || {
+        let document = web_sys::window().unwrap().document().unwrap();
+
+        let input_field = document.get_element_by_id("inputNumber")
+            .expect("#inputNumber should exist");
+        let input_field = input_field.dyn_ref::<HtmlInputElement>()
+            .expect("#inputNumber should be a HtmlInputElement");
+
+        let number = match input_field.value().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => 0,
+        };
+
+        {
+            let worker_handle = &*worker.borrow();
+            let _ = worker_handle.post_message(&number.into());
+        }
+
         console::log_1(&"Onchange callback".into());
     }) as Box<dyn FnMut()>);
 
@@ -35,6 +55,36 @@ fn setup_input_onchange_callback() {
 
     // Leaks memory
     callback.forget();
+}
+
+#[wasm_bindgen]
+pub fn log_double(number: i32) {
+    let double_number = 2*number;
+    console::log_2(&"Double input number: ".into(), &double_number.into());
+}
+
+#[wasm_bindgen]
+pub struct NumberEval {
+    number: i32,
+}
+
+#[wasm_bindgen]
+impl NumberEval {
+    pub fn new(init_number: i32) -> NumberEval {
+        NumberEval{number: init_number}
+    }
+
+    pub fn get_last_number(&self) -> i32 {
+        self.number
+    }
+
+    pub fn is_even(&mut self, number: i32) -> bool {
+        self.number = number;
+        match self.number % 2 {
+            0 => true,
+            _ => false,
+        }
+    }
 }
 
 pub fn set_panic_hook() {
--- wasm_worker_interaction/www/worker.js
+++ wasm_worker_interaction/www/worker.js
@@ -1,16 +1,22 @@
 importScripts('./pkg/wasm_worker_interaction.js');
 
-const {add_two_numbers} = wasm_bindgen;
-
-// We compiled with `--target no-modules`, which does not create a module. The generated bindings
-// can be loaded in web workers in all modern browsers.
 console.log('Hello from worker')
 
-async function run_in_worker() {
-    // Load wasm file
+const {NumberEval} = wasm_bindgen;
+
+async function init_wasm_in_worker() {
     await wasm_bindgen('./pkg/wasm_worker_interaction_bg.wasm');
 
-    console.log('3 + 5 = ', add_two_numbers(3, 5));
-}
+    var num_eval = NumberEval.new(0);
 
-run_in_worker();
+    self.onmessage = async event => {
+        console.log("Last number: ", num_eval.get_last_number());
+        if (num_eval.is_even(event.data)) {
+            console.log("Current number is even");
+        } else {
+            console.log("Current number is odd");
+        }
+    };
+};
+
+init_wasm_in_worker();
